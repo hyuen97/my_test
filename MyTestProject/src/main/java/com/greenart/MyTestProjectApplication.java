3c8b9db340d2ae2c4bd2916b7d93222eea9d6f35
hyuen97 <hyuen97@naver.com>
2021-08-09 12:48:28 +0900

    첫번째 커밋

---
+++ MyTestProject/src/main/java/com/greenart/MyTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestProjectApplication.class, args);
+	}
+
+}
